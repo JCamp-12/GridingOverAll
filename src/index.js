71d816d32926684040db6feffcafdcf674c21786
JCamp-12 <JChandlerCampbell@gmail.com>
2018-06-01 17:31:36 -0400

    functional component version of searchbar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBwBujhlQvH3lfKuGXkyrLp97Iqk234Jqc';
+
+
 
 // Create a new component
 // will produce HTML
 
 const App = () => {
-  return <div>Sup</div>;
+  return (
+  <div>
+    <SearchBar />
+  </div> 
+);
 }
 
 // have to make sure they get inserted into the DOM
